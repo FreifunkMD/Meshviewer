716904a53d55a048796b4c175b0d768f785338ad
JuS <sagurna@s3i.de>
2020-06-18 23:50:10 +0200

    Harter Schnitt. Der gesamte Content wurde gelöscht.

--- gulp/tasks/sasslint.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (gulp, plugins, config, env) {
-  return function sasslint() {
-    return gulp.src('scss/**/*.scss')
-      .pipe(plugins.sassLint())
-      .pipe(plugins.sassLint.format())
-      .pipe(env.production(plugins.sassLint.failOnError()));
-  };
-};
